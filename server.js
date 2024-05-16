ab462e11f2cd7b53ab8f797ea16e1be3639eade8
Sakai-Kaede <kana1024k@gmail.com>
2024-05-16 23:56:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,25 @@ const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 const PORT = 3000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('DBと接続中・・・');
+  }).catch((err) => {
+    console.log(err);
+  });
 
 // ルーティングが増え続けないように、ミドルウェアを使う
+app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
 
-// app.get('/', (req, res) => {
-//   res.send('hello express');
-// });
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
 
 // app.get('/users', (req, res) => {
 //   res.send('hello users');
