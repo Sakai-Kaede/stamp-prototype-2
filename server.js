f1e0aa899d5bb61802751ea16a10a87b552ec465
Sakai-Kaede <kana1024k@gmail.com>
2024-06-10 23:33:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
-const PORT = 3000;
+const uploadRoute = require('./routes/upload');
+const PORT = 5000;
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -16,9 +18,11 @@ mongoose.connect(process.env.MONGO_URL)
 
 // ルーティングが増え続けないように、ミドルウェアを使う
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/upload', uploadRoute);
 
 app.get('/', (req, res) => {
   res.send('hello express');
@@ -29,4 +33,3 @@ app.get('/', (req, res) => {
 // });
 
 app.listen(PORT, () => console.log('サーバが起動しました'));
-
